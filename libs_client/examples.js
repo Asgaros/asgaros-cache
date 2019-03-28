b65fb8672c619410d9ecb3e033d9ed7a99fd782e
Asgaros <developer@thomasbelser.net>
2019-03-28 17:13:19 +0800

    Example-code clean-up

--- libs_client/examples.js
+++ libs_client/examples.js
@@ -1,101 +1,43 @@
-function continue_processing(data) {
-    console.log(data);
-}
-
 // https://www.w3schools.com/html/html5_webstorage.asp
 $(document).ready(function() {
     $('#button-rest').click(function() {
-        var component = 'example';
-        var identifier = 'test-data';
-        var key = component+'-'+identifier;
-        var version = 0;
-
-        console.log('REST-request for: '+key);
-
-        // Try to get data from cache.
-        var data = localStorage.getItem('cached-data-'+key);
-
-        // If data found, set version of cached data.
-        if (data !== null) {
-            data = JSON.parse(data);
-            version = data.version;
-
-            console.log('Cached data found on client: Version '+version);
-        } else {
-            console.log('No cached data found on client');
-        }
-
-        // Set location.
-        var location = 'http://localhost/caching/index.php?component='+component+'&identifier='+identifier+'&version='+version;
-
-        console.log('Send request to: '+location);
-
-        $.ajax({
-            url: location
-        }).done(function(response) {
-            console.log('Response received ...');
-
-            if (response === 'up-to-date') {
-                console.log('Cached data on client up-to-date.');
-
-                console.log(data);
-            } else {
-                console.log('Update data on client.')
-
-                console.log(response);
-
-                // Parse response to JSON-object.
-                data = JSON.parse(response);
-
-                // Save data in localStorage.
-                localStorage.setItem('cached-data-'+key, response);
-
-                console.log(JSON.parse(localStorage.getItem('cached-data-'+key)));
-            }
-        });
+        rest_request_versioning();
     });
 
     // https://www.tutorialdocs.com/article/indexeddb-tutorial.html
 });
 
+function continue_processing(data) {
+    console.log(data);
+}
 
-$(document).ready(function() {
-    function client_cache_example() {
-        var data = cache_fetch('example', 'client-cache', 'data');
+function get_some_data() {
+    return 'Lorem Ipsum Dolor Sit Amet ...';
+}
 
-        if (data === false) {
-            console.log('No data in cache ...');
+function client_cache_example() {
+    var data = cache_fetch('example', 'client-cache', 'data');
 
-            var result = get_some_data();
+    if (data === false) {
+        console.log('No data in cache ...');
 
-            data = cache_store('example', 'client-cache', 'data', result);
-        }
+        var result = get_some_data();
 
-        data = cache_fetch('example', 'client-cache', 'data');
-
-        if (data !== false) {
-            console.log('Data in cache ...');
-            console.log(data);
-
-            cache_delete('example', 'client-cache', 'data');
-        }
+        data = cache_store('example', 'client-cache', 'data', result);
     }
 
-    function get_some_data() {
-        return 'Lorem Ipsum Dolor Sit Amet ...';
+    data = cache_fetch('example', 'client-cache', 'data');
+
+    if (data !== false) {
+        console.log('Data in cache ...');
+        console.log(data);
+
+        cache_delete('example', 'client-cache', 'data');
     }
-
-    //client_cache_example();
-
-    //rest_request_default();
-
-    //rest_request_caching();
-
-    //rest_request_versioning();
-});
+}
 
 function rest_request_default() {
-    var location = 'http://localhost/caching/index.php?component=example&identifier=test-data';
+    var location = 'http://localhost/wordpress/wp-content/plugins/asgaros-cache/index.php?component=example&identifier=test-data';
 
     $.ajax({
         url: location
@@ -109,7 +51,7 @@ function rest_request_caching() {
     var data = cache_fetch('example', 'test-data', 'data');
 
     if (data === false) {
-        var location = 'http://localhost/caching/index.php?component=example&identifier=test-data';
+        var location = 'http://localhost/wordpress/wp-content/plugins/asgaros-cache/index.php?component=example&identifier=test-data';
 
         $.ajax({
             url: location
@@ -131,16 +73,20 @@ function rest_request_versioning() {
         version = data.version;
     }
 
-    var location = 'http://localhost/caching/index.php?component=example&identifier=test-data&version='+version;
+    var location = 'http://localhost/wordpress/wp-content/plugins/asgaros-cache/index.php?component=example&identifier=test-data&version='+version;
 
     $.ajax({
         url: location
     }).done(function(response) {
+        continue_processing('Response:');
+        continue_processing(response);
+
         if (response !== 'up-to-date') {
             response = JSON.parse(response);
             data = cache_store('example', 'test-data', 'data', response.data, response.version);
         }
 
+        continue_processing('Cached Data:');
         continue_processing(data);
     });
 }
